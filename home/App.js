710923b87cb4cb57df2bf620e3b970f4fefe6f86
IldoRamos <ildoramosvieira@gmail.com>
2022-04-11 20:59:26 -0300

    updated project

--- home/App.js
+++ home/App.js
@@ -1,24 +1,63 @@
 import React from 'react'
-import {Text, View} from 'react-native'
-import GetCampos from './campos'
+import {Text, View, Image, TextInput, TouchableOpacity, StyleSheet} 
+from 'react-native'
+
 
 const TelaInicial = ()=>{
     return(
-    <View style={
-        {
-            flexDirection:'row',
-            height: 100,
-            alignText: 'center',
-            padding: 20,
-            fontSize: 35,
-            backgroundColor: '#f0f'
-        }
+    <View style={style.backgroud
+       
     }>        
-        <Text> Seja Bem vindo</Text>
-        <GetCampos/>
+    <Image
+        source={require('./src/img/logo.png')}
+        style={{width: '80%'}}
+        resizeMode='center'
+    />
+    <TextInput
+        style={style.input}
+        placeholder='Usuario'
+        autoCorrect={false}
+        onChangeText={()=>{}}>
+    </TextInput>
+
+    <TextInput
+        style={style.input}
+        placeholder='Senha'
+        autoCorrect={false}
+        onChangeText={()=>{}}>
+    </TextInput>
+
+    <TouchableOpacity>
+        <Text>Logar</Text>
+    </TouchableOpacity>
+        
     </View>
     
     ) 
 };
 
+const style = StyleSheet.create({
+    backgroud:{
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#19cc'
+    },
+    input:{
+        
+        width: 300,
+        margin: 5,
+        backgroundColor: '#fff',
+        justifyContent: 'center'
+    },
+
+    container :{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '90%',
+        paddingBottom: 20
+
+    }
+});
 export default TelaInicial;
